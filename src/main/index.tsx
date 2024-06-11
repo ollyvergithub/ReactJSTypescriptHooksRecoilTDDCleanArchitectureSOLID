88899dc688cacae9c626cfd2dd924a1881755b92
ollyvergithub <ollyverottoboni@gmail.com>
2024-06-11 12:48:48 -0300

    refactor: voltando a 15. - Add Index File multiple imports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@/presentation/components'
+import { Login } from '@/presentation/pages'
 
 ReactDOM.render(
-  <Router />,
+  <Login />,
   document.getElementById('main')
 )
