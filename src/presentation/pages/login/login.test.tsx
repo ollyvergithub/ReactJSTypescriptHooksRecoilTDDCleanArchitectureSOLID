2db47defdbcffb2c7cb60f5a82cdad5f43abd4c3
ollyvergithub <ollyverottoboni@gmail.com>
2024-06-12 09:37:44 -0300

    test: 18. Testando o estado inicial do Login 2/2 - Teste btn submit disabled inicialmente

--- src/presentation/pages/login/login.test.tsx
+++ src/presentation/pages/login/login.test.tsx
@@ -8,4 +8,10 @@ describe('Login Page', () => {
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
+
+  test('O botão de submit deve estar desabilitado até o preenchimento correto dos campos do form', () => {
+    const { getByTestId } = render(<Login/>)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
+  })
 })
