77b76a78f5dc617fd68d674eacd05f8cff845c9d
ollyvergithub <ollyverottoboni@gmail.com>
2024-06-12 10:09:38 -0300

    refactor: 19. Testando a integração com o Validation - move SUT creation a Factory method

--- src/presentation/pages/login/login.test.tsx
+++ src/presentation/pages/login/login.test.tsx
@@ -1,27 +1,38 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
 
 describe('Login Page', () => {
   test('Não deve exibir Spinner ou Mensagem de Erro ao iniciar a página', () => {
-    const { getByTestId } = render(<Login/>)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = makeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
 
   test('O botão de submit deve estar desabilitado até o preenchimento correto dos campos do form', () => {
-    const { getByTestId } = render(<Login/>)
+    const { sut } = makeSut()
     // as HTMLButtonElement cria um cache do elemento e com isso temos acessa ao .disabled
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
   })
 
   test('Deve validar o estado inicial dos Spans dos Inputs Email e Senha', () => {
-    const { getByTestId } = render(<Login/>)
-    const emailStatus = getByTestId('email-status')
+    const { sut } = makeSut()
+    const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
-    const passwordStatus = getByTestId('password-status')
+    const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
   })
